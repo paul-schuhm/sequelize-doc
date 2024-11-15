25a30ae73f3a4b4c93bf9bcb33f036ec902f6e93
Theo22100 <theoguerinBZH@hotmail.com>
2024-11-15 13:47:31 +0100

    Bien aussi

--- index.js
+++ index.js
@@ -8,6 +8,61 @@ const sequelize = new Sequelize("demo", "root", "password", {
 });
 
 
+// Fonction principale
+async function main() {
+  try {
+    // Tester connexion
+    await sequelize.authenticate();
+    console.log("Connexion établie avec succès.");
+
+    // Synchroniser les modèles avec BDD
+    await sequelize.sync({ force: true }); //On force la synchronisation pour réinitialiser la BDD
+    console.log("Modèles synchronisés.\n\n\n");
+
+    // Ajouter des utilisateurs
+    const user1 = await User.create({
+      name: "Alice",
+      email: "alice@example.com",
+    });
+    const user2 = await User.create({ name: "Bob", email: "bob@example.com" });
+    console.log("Ajouts des utilisateurs...\n");
+    console.log("Utilisateurs ajoutés :", [user1.name, user2.name],"\n\n");
+
+    //Récupérer un utilisateur avec Primary Key
+    const usersOnePK = await User.findByPk(1);
+    if (usersOnePK === null) {
+      console.log("L'utilisateur n'est pas trouvé !\n");
+    } else {
+      console.log("Utilisateur trouvé via PK :",usersOnePK instanceof User); // true
+      console.log("Nom :",usersOnePK.name,"\n\n"); // Renvoie Bob
+    }
+
+    //Récupérer un utilisateur qui s'appelle Bob
+    const userOne = await User.findOne({ where: { name: "Bob" } });
+    if (userOne === null) {
+      console.log("L'utilisateur n'est pas trouvé !\n\n");
+    } else {
+      console.log("Utilisateur trouvé via son Nom :",userOne instanceof User); // true
+      console.log("Nom :",userOne.name,"\n\n"); // Renvoie Bob
+    }
+
+    // Récupérer et afficher tous les utilisateurs
+    const usersFindAll = await User.findAll();
+    console.log("Liste des utilisateurs :");
+    usersFindAll.forEach((user) =>
+      console.log(`- ${user.name} (${user.email})`)
+    );
+    console.log("\n\n FINI");
+    
+  } catch (error) {
+    console.error("Erreur lors de l'exécution :", error);
+  } finally {
+    // Fermer la connexion
+    await sequelize.close();
+  }
+}
+
+
 // Modèle User
 const User = sequelize.define("User", {
   name: {
@@ -19,33 +74,5 @@ const User = sequelize.define("User", {
     unique: true,
   },
 });
-
-// Fonction principale
-async function main() {
-  try {
-    // Tester connexion
-    await sequelize.authenticate();
-    console.log("Connexion établie avec succès.");
-
-    // Synchroniser les modèles avec BDD
-    await sequelize.sync({ force: true }); //On force la synchronisation pour réinitialiser la BDD
-    console.log("Modèles synchronisés.");
-
-    // Ajouter des utilisateurs
-    const user1 = await User.create({ name: "Alice", email: "alice@example.com"});
-    const user2 = await User.create({ name: "Bob", email: "bob@example.com" });
-    console.log("Utilisateurs ajoutés :", [user1.name, user2.name]);
-
-    // Récupérer et afficher les utilisateurs
-    const users = await User.findAll();
-    console.log("Liste des utilisateurs :");
-    users.forEach((user) => console.log(`- ${user.name} (${user.email})`));
-  } catch (error) {
-    console.error("Erreur lors de l'exécution :", error);
-  } finally {
-    // Fermer la connexion
-    await sequelize.close();
-  }
-}
 
 main();
